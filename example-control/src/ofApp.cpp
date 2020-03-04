fde94cfdd273b35c885009bd138d2c7e8474d8db
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-03-03 16:01:05 -0800

    add basic example

--- example-control/src/ofApp.cpp
+++ example-control/src/ofApp.cpp
@@ -2,17 +2,35 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    input.registerInputs(patchbay);
+    patchbay.registerControllable1f("rect-size", [&] (float value) {
+        rect.setSize(200*value, 200*value);
+    });
+    patchbay.registerControllable1f("rect-color", [&] (float value) {
+        color = ofColor(255*value, 255, 255);
+    });
+    patchbay.registerControllable1f("rect-scale", [&] (float value) {
+        scale = 0.5 + 10*value;
+    });
+
+    patchbay.connect1f("mouse-x", "rect-size");
+    patchbay.connect1f("mouse-y", "rect-color");
+
+    patchbay.connect1f("audio-rms", "rect-scale");
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    patchbay.process();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    ofBackground(0);
+    ofSetColor(color);
+    ofDrawRectangle(rect.x - scale*rect.width/2, rect.y - scale*rect.height, scale*rect.width, scale*rect.height);
 }
 
 //--------------------------------------------------------------
--- example-control/src/ofApp.h
+++ example-control/src/ofApp.h
@@ -1,24 +1,31 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxPatchbayInput.h"
+#include "ofxPatchbay.h"
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    void setup();
+    void update();
+    void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    ofRectangle rect;
+    float scale;
+    ofColor color = ofColor(255,0,0);
+    ofxPatchbay patchbay;
+    ofxPatchbayInput input;
 };
